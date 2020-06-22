2196a1f552315b518323732f71c0d47434583deb
SPV <spiros_vigos7@hotmail.com>
2020-06-22 19:51:03 +0300

    Gerate API-url by entered zip

--- website/app.js
+++ website/app.js
@@ -1,5 +1,39 @@
 /* Global Variables */
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=93ec3247177dbb56166d5141dcdca068';
+const generate = document.getElementById('generate');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// GET Info from Api using async function and 
+
+const getWeatherData = async (url) => {
+
+    const request = await fetch(url);
+    console.log(request);
+    try{
+        const weatherData = await request.json();
+
+        console.log(weatherData);
+    }
+    catch(error){
+        console.log("error", error)
+
+    }
+    
+}
+
+//add event listener to "Generate" button
+generate.addEventListener('click', genFunction)
+
+//Async function to get the input and call the api data
+async function genFunction(){
+    
+    zip = document.getElementById("zip").value;
+    const apiUrl = url + zip + apiKey;
+    getWeatherData(apiUrl);
+
+
+}
